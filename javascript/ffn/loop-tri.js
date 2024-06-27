dbcafebbba1e4f2bc95f713f2554baad7c4cb98d
levywanke <levywanke@gmail.com>
2024-06-27 13:09:22 +0300

    loop

---
+++ javascript/ffn/loop-tri.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+//use for loop to iterate
+for(let tri = 0; tri <= 8; tri++){
+	//output using console.log command
+	//use repeat() function to loop the triangle
+	console.log('#'.repeat(tri));
+}
