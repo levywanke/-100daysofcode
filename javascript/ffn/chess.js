faa5a3fe4ed9e93ebb8204b14bff90a00ba0f25e
levywanke <levywanke@gmail.com>
2024-06-27 13:02:46 +0300

    js file

--- javascript/ffn/chess.js
+++ javascript/ffn/chess.js
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
+//defines the size of the chessboard = 8
 let size = 8;
+//main loop having row and column
 for (let row = 0; row < size; row++){
+	//declare a boardrow
 	let boardRow = '';
+	//declare column
 	for (let col = 0; col < size; col++){
+		//use conditional if....else
 		if ((row + col) % 2 == 0){
 			boardRow += ' ';
 		}
@@ -9,5 +15,6 @@ for (let row = 0; row < size; row++){
 			boardRow += '#'
 		}
 	}
+	//output using console.log command
 	console.log(boardRow);
 }
