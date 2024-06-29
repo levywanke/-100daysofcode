b2a8370675c7c89ef4bd67907a163057e8dfeddc
levywanke <levywanke@gmail.com>
2024-06-29 12:49:21 +0300

    java array

---
+++ java/WORA/for-arr.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String []args){
+        int arr[] ={12,34,45,56,876,56,78,8,9,98,99,65,45,89,90};
+         for (int i = 0; i<arr.length;i++){
+       
+        System.out.println(arr[i]);
+         }
+    }
+}
